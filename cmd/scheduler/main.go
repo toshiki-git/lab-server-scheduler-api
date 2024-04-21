0bd86b1c6252a262cc5811c31b6ef35551d665ac
Toshiki Takakubo <219tosiki@gmail.com>
2024-04-21 19:30:58 +0900

    feat: add sample code for http server

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hoge(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hoge")
+}
+
+func fuga(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "fuga")
+}
+
+func main() {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+
+	http.HandleFunc("/hoge", hoge)
+	http.HandleFunc("/fuga", fuga)
+
+	server.ListenAndServe()
+}
