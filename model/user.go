d7fb8ec757307f0f84d1aba34c74ee8358c4cf03
Toshiki Takakubo <219tosiki@gmail.com>
2024-05-06 18:32:12 +0900

    feat: Add image_url, created_at, and updated_at fields to User model

--- model/user.go
+++ model/user.go
@@ -1,7 +1,10 @@
 package model
 
 type User struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	ImageURL  string `json:"image_url"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
 }
