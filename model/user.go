3ed5e5bfabcb07b016db5a14f5529416ee5de1f0
Toshiki Takakubo <219tosiki@gmail.com>
2024-05-06 18:34:34 +0900

    Refactor User model to use "UserId" instead of "Id" for consistency

--- model/user.go
+++ model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	Id        string `json:"id"`
+	UserId    string `json:"user_id"`
 	Name      string `json:"name"`
 	Email     string `json:"email"`
 	ImageURL  string `json:"image_url"`
