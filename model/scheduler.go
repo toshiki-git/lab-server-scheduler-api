af088b85cec567bc1f34cfc54f9311f480ca971d
Toshiki Takakubo <219tosiki@gmail.com>
2024-05-06 18:32:54 +0900

    feat: Add Scheduler model

---
+++ model/scheduler.go
@@ -0,0 +1,12 @@
+package model
+
+type Scheduler struct {
+	ReservationId string `json:"reservation_id"`
+	UserId        string `json:"user_id"`
+	Title         string `json:"title"`
+	IsAllDay      bool   `json:"is_all_day"`
+	StartTime     string `json:"start_time"`
+	EndTime       string `json:"end_time"`
+	CreatedAt     string `json:"created_at"`
+	UpdatedAt     string `json:"updated_at"`
+}
