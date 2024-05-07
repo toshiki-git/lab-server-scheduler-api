594b49eba2b95216524dafff085711e045893723
Toshiki Takakubo <219tosiki@gmail.com>
2024-05-07 15:48:40 +0900

    feat: Add CORS middleware to enable Cross-Origin Resource Sharing

---
+++ middleware/cors.go
@@ -0,0 +1,9 @@
+package middleware
+
+import "net/http"
+
+func EnableCORS(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
